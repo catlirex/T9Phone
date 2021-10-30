4d4a86a557f04cb07763e36e8763fed66c35aca6
catherine-swyvl <catherine.uk.20@gmail.com>
2021-10-30 21:14:47 +0100

    Build: basic store setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { modeStore } from "./store/predictStore";
+import { messageStore } from "./store/messageStore";
 
 function App() {
   return (
     <div className="App">
-      <h1>T9 Phone Input demo</h1>
+      <div className="wrapper">
+        <h1>T9 phone demo input</h1>
+        <div className="input-mode">
+          <button>Non Predict Mode</button>
+          <button>Predict Mode</button>
+        </div>
+
+        <div className="phone"></div>
+      </div>
     </div>
   );
 }
