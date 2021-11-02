a82763cf1db6b06f1c12c906bb1ffca67c1ca6b3
catherine-swyvl <catherine.uk.20@gmail.com>
2021-11-02 00:17:04 +0000

    cleanup const data

--- client/src/components/Keyboard/Keyboard.jsx
+++ client/src/components/Keyboard/Keyboard.jsx
@@ -6,25 +6,7 @@ import {
   getPredictWord,
 } from "../../service/translate/translate.service";
 
-const TOP_FUNCTION = ["reset", "next", "backspace"];
-const NUM_KEYBOARD = [
-  [
-    { value: null, display: 1 },
-    { value: 2, display: "2 abc" },
-    { value: 3, display: "3 def" },
-  ],
-  [
-    { value: 4, display: "4 ghi" },
-    { value: 5, display: "5 jkl" },
-    { value: 6, display: "6 mno" },
-  ],
-  [
-    { value: 7, display: "7 pqrs" },
-    { value: 8, display: "8 tuv" },
-    { value: 9, display: "9 wxyz" },
-  ],
-];
-const BOTTOM_FUNCTION = ["*", " ", "#"];
+import { TOP_FUNCTION, NUM_KEYBOARD, BOTTOM_FUNCTION } from "./keyboardSetup";
 
 export default function KeyBoard() {
   const predictMode = useStore((state) => state.predictMode);
---
+++ client/src/components/Keyboard/keyboardSetup.js
@@ -0,0 +1,19 @@
+export const TOP_FUNCTION = ["reset", "next", "backspace"];
+export const NUM_KEYBOARD = [
+  [
+    { value: null, display: 1 },
+    { value: 2, display: "2 abc" },
+    { value: 3, display: "3 def" },
+  ],
+  [
+    { value: 4, display: "4 ghi" },
+    { value: 5, display: "5 jkl" },
+    { value: 6, display: "6 mno" },
+  ],
+  [
+    { value: 7, display: "7 pqrs" },
+    { value: 8, display: "8 tuv" },
+    { value: 9, display: "9 wxyz" },
+  ],
+];
+export const BOTTOM_FUNCTION = ["*", " ", "#"];
